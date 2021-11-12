237eca80c82937cbe05ed984d65f3a60019fb97d
Alex Georgescu <punisherx@gmail.com>
2021-11-12 16:09:05 +0100

    Initial Setup SpringBoot + Angular13

---
+++ src/main/java/fr/georgescu/teilen/TeilenApplication.java
@@ -0,0 +1,13 @@
+package fr.georgescu.teilen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeilenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeilenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/georgescu/teilen/TeilenApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.georgescu.teilen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeilenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
